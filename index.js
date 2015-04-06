1551faa279011d6c7f823fb7f08d750f21a4dc20
Niels Krijger <niels@kryger.nl>
2015-04-06 16:36:42 +0200

    Renamed LinksBuilder to Links

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var errors = require('./lib/RestErrors.js');
-var LinksBuilder = require('./lib/LinksBuilder.js');
+var RestErrors = require('./lib/RestErrors.js');
+var Links = require('./lib/Links.js');
 var Collection = require('./lib/Collection.js');
 
 // Expose all errors directly in the module export
-for (var property in errors) {
-    module.exports[property] = errors[property];
+for (var property in RestErrors) {
+    module.exports[property] = RestErrors[property];
 }
 
-module.exports.LinksBuilder = LinksBuilder;
+module.exports.Links = Links;
 module.exports.Collection = Collection;
