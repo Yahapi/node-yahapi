f149a50b3d0fcd2f801d6a74214bd4ae3ad133a2
Niels Krijger <niels@kryger.nl>
2015-04-06 14:23:59 +0200

    Added unauthorized error (401)

--- test/RestErrors.js
+++ test/RestErrors.js
@@ -6,7 +6,7 @@ var errors = require('../index').errors;
 
 describe('RestErrors', function() {
 
-    it('should return a valid ValidationError with status 422', function() {
+    it('should return a valid ValidationError with HTTP status 422', function() {
         var error = new errors.ValidationError('Invalid message!');
         assert.instanceOf(error, Error);
         assert.deepEqual(error, {
@@ -14,6 +14,16 @@ describe('RestErrors', function() {
             code: 'validation-error',
             message: 'One or more request parameters are invalid',
             errors: 'Invalid message!'
+        });
+    });
+
+    it('should return a valid UnauthorizedError with HTTP status 401', function() {
+        var error = new errors.UnauthorizedError();
+        assert.instanceOf(error, Error);
+        assert.deepEqual(error, {
+            status: 401,
+            code: 'unauthorized-error',
+            message: 'Missing or invalid user credentials'
         });
     });
 });
