91cf3dd77bba88e469dc1ac36948a5c0f558d994
Niels Krijger <niels@kryger.nl>
2015-05-19 17:08:29 +0200

    Added item representation function

--- test/Collection.js
+++ test/Collection.js
@@ -10,9 +10,21 @@ describe('Collection', function () {
 
     beforeEach(function() {
         items = [];
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 5; i++) {
             items.push({ num: i });
         }
+    });
+
+    describe('items', function() {
+        it('should transform items', function() {
+            var func = function(elm) {
+                return { num: elm.num * 2 }
+            }
+            var result = new Collection('http://www.example.org/test/12345?limit=3&offset=12', items).transform(func).build();
+            assert.deepEqual(result.items, [
+                { num: 0 }, { num: 2 }, { num: 4 }, { num: 6 }, { num: 8 }
+            ]);
+        });
     });
 
     describe('links', function() {
@@ -48,7 +60,7 @@ describe('Collection', function () {
             assert.deepEqual(result.meta, {
                 limit: 3,
                 offset: 12,
-                size: 10,
+                size: 5,
                 total: 320
             });
         });
@@ -72,7 +84,7 @@ describe('Collection', function () {
                 elm4: 4,
                 limit: 3,
                 offset: 0,
-                size: 10,
+                size: 5,
                 total: 3
             });
         });
